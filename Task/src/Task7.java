82f7bcea02b3b7d5f83f77fe07d218af1d2ff1fd
yuriybocharov <30383383+yuriybocharov@users.noreply.github.com>
2017-08-02 15:22:19 +0300

    Update Task7.java

--- Task/src/Task7.java
+++ Task/src/Task7.java
@@ -2,4 +2,21 @@
  * Created by Ura on 02.08.2017.
  */
 public class Task7 {
+  //Ввести число, определить каким числам оно кратно.
+  public static void main(String[] args) {
+
+        Scanner num = new Scanner (System.in);
+        int a;
+        System.out.println ("Введите число");
+        a = num.nextInt ();
+
+        System.out.println ("Число " + a + " кратно: ");
+
+        for (int i = a; i > 0; i--) {
+            if (a % i == 0) {
+                System.out.println (i);
+
+            }
+        }
+    }
 }
