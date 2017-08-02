a63f7b95889121bc5a2e5a1e1a1ca01fc2ade781
yuriybocharov <30383383+yuriybocharov@users.noreply.github.com>
2017-08-02 15:24:39 +0300

    Update Task8.java

--- Task/src/Task8.java
+++ Task/src/Task8.java
@@ -2,4 +2,23 @@
  * Created by Ura on 02.08.2017.
  */
 public class Task8 {
+  //Реализовать функцию по нахождению сумм цифр в числе без использования класса String или массивов типа char
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println ("Введите число");
+
+        int num = scanner.nextInt ();
+        int num1 = num % 10;
+        System.out.println (num1);
+        int num2 = (num % 100) / 10;
+        System.out.println (num2);
+        int num3 = (num % 1000) / 100;
+        System.out.println (num3);
+       
+
+        int result = num3 + num2 + num1;
+
+        System.out.println ("Сумма цифр числа: " + num + " равна " + result);
+    }
 }
