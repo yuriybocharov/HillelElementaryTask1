a1c432d77eb02f136f413ac3fef6eb6c7cce6ad4
yuriybocharov <30383383+yuriybocharov@users.noreply.github.com>
2017-08-02 15:14:31 +0300

    Update Task1.java

--- Task/src/Task1.java
+++ Task/src/Task1.java
@@ -2,4 +2,16 @@
  * Created by Ura on 02.08.2017.
  */
 public class Task1 {
+  public static void main(String[] args) {
+        //Среднее арифметическое значение двух чисел.
+
+        Scanner scanner = new Scanner (System.in);
+
+        double a = scanner.nextDouble ();
+        double b = scanner.nextDouble ();
+
+        double resalt = (a + b) / 2;
+        System.out.println (resalt);
+    }
+}
 }
