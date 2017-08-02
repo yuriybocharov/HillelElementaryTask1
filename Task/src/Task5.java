f85ec7509a90f89bcb9288a73d0e4ee3a3afaefa
yuriybocharov <30383383+yuriybocharov@users.noreply.github.com>
2017-08-02 15:19:46 +0300

    Update Task5.java

--- Task/src/Task5.java
+++ Task/src/Task5.java
@@ -2,4 +2,23 @@
  * Created by Ura on 02.08.2017.
  */
 public class Task5 {
+  //Ввести число, определить четное или нет.
+  public static void main(String[] args) {
+        Scanner num = new Scanner (System.in);
+        int a;
+
+        for (int i = 0; i < 10; i++) {
+
+
+            System.out.println ("Enter first num: ");
+            a = num.nextInt ();
+
+            if (a % 2 == 0) {
+                System.out.println ("Even number");
+
+            } else {
+                System.out.println ("Odd number");
+            }
+        }
+    }
 }
