66f4a410c1c1654d06f8e8fe29c42e2e14003bdd
yuriybocharov <30383383+yuriybocharov@users.noreply.github.com>
2017-08-02 15:15:28 +0300

    Update Task2.java

--- Task/src/Task2.java
+++ Task/src/Task2.java
@@ -2,4 +2,24 @@
  * Created by Ura on 02.08.2017.
  */
 public class Task2 {
+  public static void main(String[] args) {
+//среднее арифметическое значение произвольного количества чисел.
+        Scanner scanner = new Scanner (System.in);
+
+        int a;
+        double c = 0;
+        double result, b;
+        System.out.println ("Какое количество чисел вы хотите ввести: ");
+        a = scanner.nextInt ();
+        System.out.println ("Введите " + a + " чисел");
+
+        for (int i = 0; i < a; i++) {
+            System.out.print ("Введите " + (i + 1) + " число: ");
+            b = scanner.nextDouble ();
+            c += b;
+        }
+        result = c / a;
+        System.out.println ("среднее арифметическое значение " + result);
+    }
+
 }
