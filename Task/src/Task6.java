24627a046dfbf58902e58196613147e3f77074a0
yuriybocharov <30383383+yuriybocharov@users.noreply.github.com>
2017-08-02 15:21:10 +0300

    Update Task6.java

--- Task/src/Task6.java
+++ Task/src/Task6.java
@@ -2,4 +2,26 @@
  * Created by Ura on 02.08.2017.
  */
 public class Task6 {
+  //Ввести число, определить простое ли оно
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        int a, b;
+
+        System.out.println ("Введите число");
+
+
+        for (int n = 0; n < 10; n++) {
+            b = 0;
+            a = scanner.nextInt ();
+            for (int i = 1; i <= a; i++) {
+                if (a % i == 0) b++;
+            }
+            if (b <= 2) {
+                System.out.println ("Простое");
+            } else {
+                System.out.println ("нет");
+            }
+        }
+
+    }
 }
